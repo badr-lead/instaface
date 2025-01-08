f738e1c7e7624292f6161f0f8fd6b700f5f77913
guest badr <sieurbadr@gmail.com>
2025-01-08 21:06:49 +0100

    feat(liker): Création de la fonctionne de Liker

--- src/app/insta-face/insta-face.component.ts
+++ src/app/insta-face/insta-face.component.ts
@@ -14,12 +14,38 @@ export class InstaFaceComponent implements OnInit {
   createdAt! : Date;
   likes! : number;
   imageUrl! : string
+  aimerBouttonTexte! : string
+  utilisateurDejaAime! : boolean
 
   ngOnInit(): void {
     this.title = 'Archibald';
     this.description = 'Mon meilleur ami depuis toujours';
     this.createdAt = new Date();
     this.likes = 0;
-    this.imageUrl! = 'https://picsum.photos/id/13/300/'
+    this.imageUrl! = 'https://picsum.photos/id/192/300/'
+    this.aimerBouttonTexte = 'J\'aime'
+    this.utilisateurDejaAime = false
   }
+
+  onLike() {
+    if (this.utilisateurDejaAime) {
+      this.dislike()
+    }
+    else {
+      this.like()
+    } 
+  }
+
+  dislike() {
+    this.likes--;
+    this.aimerBouttonTexte = 'J\'aime';
+    this.utilisateurDejaAime = false
+  }
+
+  like() {
+    this.likes++;
+    this.aimerBouttonTexte = 'J\'aime pas';
+    this.utilisateurDejaAime = true
+  }
+
 }
